e81f4833607376da6c13d58e90399030b9785b61
mmmdddfff <dallefratte.marco@gmail.com>
2023-11-17 11:50:48 +0100

    add README animation

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Thank you for your time ...and vice versa.
 Permanent analog clock ⏱
+
+![Thank you for your time ...and vice versa](./src/lib/readme.gif)
--- dist/index.bd2bc781.js
+++
@@ -1,3 +0,0 @@
-!function(){document.querySelector(".hours");let e=document.querySelector(".second"),t=document.querySelector(".minute"),r=document.querySelector(".hour");function o(){let o=new Date,n=o.getSeconds(),s=o.getMinutes(),u=o.getHours();e.style.transform=`translateX(-50%) rotate(${6*n+360*s}deg)`,t.style.transform=`translateX(-50%) rotate(${6*s+n/12}deg)`,r.style.transform=`translateX(-50%) rotate(${30*u+s/12*6}deg)`}setInterval(o,1e3),o()}();//# sourceMappingURL=index.bd2bc781.js.map
-
-//# sourceMappingURL=index.bd2bc781.js.map
